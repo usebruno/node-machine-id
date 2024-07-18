4b2f12ea0a7b0d15f8a8d224efbb872221467452
lohit-1 <lohit@usebruno.com>
2024-07-18 12:21:23 +0530

    updates

--- tests/backwardCompatibility.js
+++ tests/backwardCompatibility.js
@@ -3,21 +3,21 @@ const assert = chai.assert;
 const { machineId, machineIdSync } = require("../index.js");
 const nodeMachineId = require("node-machine-id");
 
-const ogMachineId = nodeMachineId.machineId;
-const ogMachineIdSync = nodeMachineId.machineIdSync;
+const originalMachineId = nodeMachineId.machineId;
+const originalMachineIdSync = nodeMachineId.machineIdSync;
 
 describe("Backward Compatibilty: machineId({original: true})", function () {
   it("node-machine-id and @usebruno/node-machine-id should return the same device ids", async () => {
     let id = await machineId({ original: true });
-    let ogId = await ogMachineId({ original: true });
-    assert.equal(id, ogId);
+    let originalId = await originalMachineId({ original: true });
+    assert.equal(id, originalId);
   });
 });
 
 describe("Backward Compatibilty: machineIdSync({original: true})", function () {
   it("node-machine-id and @usebruno/node-machine-id should return the same device ids", async () => {
     let id = machineIdSync({ original: true });
-    let ogId = ogMachineIdSync({ original: true });
-    assert.equal(id, ogId);
+    let originalId = originalMachineIdSync({ original: true });
+    assert.equal(id, originalId);
   });
 });
